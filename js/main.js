b696e5d723fd481a0c4faf8d1f1590a504969f6b
Akahibe Kingsley <divine10646@gmail.com>
2021-07-09 20:39:23 +0100

    txn failed alert

--- js/main.js
+++ js/main.js
@@ -6,13 +6,6 @@ function closeMenu() {
     document.getElementById("header-menu").style.height = "0%";
 }
 
-var mealoptions = [
-    "Bread Sandwich (mini)",
-    "Bread Sandwich (midi)",
-    "Bread Sandwich (maxi)",
-    "Bread Sandwich Specials (Lala Specials)",
-    "Lala noodles"
-]
 var mealdescriptions = [
     "Mini-sized bread with salad and eggs",
     "Medium-sized bread with salad and eggs",
@@ -36,12 +29,21 @@ $(document).ready(function () {
 
 $("#paymentForm").submit(function (event) {
     event.preventDefault(event);
+    var mealoptions = [
+        "Bread Sandwich (mini)",
+        "Bread Sandwich (midi)",
+        "Bread Sandwich (maxi)",
+        "Bread Sandwich Specials (Lala Specials)",
+        "Lala noodles"
+    ]
     var firstname = $("#firstname").val();
     var lastname = $("#lastname").val();
     var email = $("#email").val();
+    var order = $("#selectfood").val();
+    order = mealoptions[order - 1];
     var amount = $("#amount").val();
     var txnId = Date.now();
-    payWithPaystack(email, amount, txnId);
+    payWithPaystack(email, amount, txnId, firstname, lastname, order, amount);
 })
 
 function generateTxnId() {
@@ -57,37 +59,50 @@ function reverseOrderCode(orderCode) {
     txnId = parseInt(orderCode,16);
 }
 
-function payWithPaystack(email, amount, reference) {
+function payWithPaystack(email, amount, reference, firstname, lastname, order, amount) {
 
   var handler = PaystackPop.setup({
 
     key: 'pk_test_b201960416954ffb959c3ed057167bc6713d25a1', // Replace with your public key
-
     email: email,
-
     amount: amount * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
-
     currency: 'NGN', // Use GHS for Ghana Cedis or USD for US Dollars
-
     ref: reference, // Replace with a reference you generated
-
+    metadata: {
+        custom_fields: [
+            {
+                display_name: "Name",
+                variable_name: "name",
+                value: firstname + " " + lastname
+            },
+            {
+                display_name: "Transaction ID",
+                variable_name: "txn_id",
+                value: reference
+            },
+            {
+                display_name: "Order",
+                variable_name: "order",
+                value: order
+            }
+        ]
+    },
     callback: function(response) {
 
-      //this happens after the payment is completed successfully
+        //this happens after the payment is completed successfully
 
         var reference = response.reference;
         $("#txnid").html(reference);
         $("#txn-success").css('display', 'block');
         //alert('Payment complete! Reference: ' + reference);
 
-      // Make an AJAX call to your server with the reference to verify the transaction
+        // Make an AJAX call to your server with the reference to verify the transaction
 
     },
 
-    onClose: function() {
-
-      alert('Transaction was not completed, window closed.');
-
+      onClose: function () {
+        $("#txn-failed").css('display', 'block');
+        //alert('Transaction was not completed, window closed.');
     },
 
   });
